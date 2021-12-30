0e2bb9fe3c7c776d3349a3f20c0a890697bc4b92
Alpha <mavamalonga.alpha@gmail.com>
2021-12-30 03:30:31 +0100

    upgrade contributors process

--- app/views.py
+++ app/views.py
@@ -32,46 +32,63 @@ class ContributorProject(APIView):
 
 	def get(self, request, project_id):
 		try:
+			print("11111111111111111")
 			project = models.Project.objects.filter(id=project_id)
-			auth_user = models.Contributor.objects.filter(project_id=project_id).filter(
-				user_id=request.user.id).count()
-			if auth_user != 0 or request.user.username == 'admin':
+			print("2222222222222222222222")
+			if project.count() != 0:
 				contributors = models.Contributor.objects.filter(project_id=project_id)
+				if contributors.count() != 0:
+					serializer = serializers.ContributorSerializer(contributors, many=True)
+					return Response(serializer.data)
+				content = {"detail":"Project has no contributors"}
+				return Response(content, status=status.HTTP_404_NOT_FOUND)
 			else:
-				content = {"detail":"you are not a contributor"}
-				return Response(content)
+				content = {"detail":"Not found project with this id"}
+				return Response(content, status=status.HTTP_404_NOT_FOUND)
 		except:
 			return Response(status=status.HTTP_404_NOT_FOUND)
-		serializer = serializers.ContributorSerializer(contributors, many=True)
-		return Response(serializer.data)
+
 
 	def post(self, request, project_id):
 		try:
 			project = models.Project.objects.filter(id=project_id)
+			if project.count() != 0:
+				if request.user.username == 'admin':
+					serializer = serializers.AddContributorSerializer(data=request.data)
+					if serializer.is_valid():
+						serializer.save()
+						return Response(serializer.data, status.HTTP_201_CREATED)
+					return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+				else:
+					content = {"detail":"only admin acount can add new contributors"}
+					return Response(content)
+			else:
+				content = {"detail":"Not found project with this id"}
+				return Response(content, status=status.HTTP_404_NOT_FOUND)
+
 		except:
 			return Response(status=status.HTTP_404_NOT_FOUND)
-		serializer = serializers.AddContributorSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	def delete(self, request, project_id):
 		try:
 			project = models.Project.objects.filter(id=project_id).count()
-			if project != 0 and request.user.username == 'admin':
-				contributor_deleted = models.Contributor.objects.filter(project_id=project_id).filter(
-				user_id=request.data['user_id'])
-				if contributor_deleted.count() != 0:
-					contributor_deleted.delete()
-					content = {"detail": f"contributor user_id {request.data['user_id']} deleted"}
-					return Response(content)
+			if project.count() != 0:
+				if request.user.username == 'admin':
+					contributor_deleted = models.Contributor.objects.filter(project_id=project_id).filter(
+					user_id=request.data['user_id'])
+					if contributor_deleted.count() != 0:
+						contributor_deleted.delete()
+						content = {"detail": f"contributor user_id {request.data['user_id']} deleted"}
+						return Response(content)
+					else:
+						content = {"detail":"No found contributor with this id"}
+						return Response(content, status=status.HTTP_404_NOT_FOUND)
 				else:
-					content = {"detail":"No found contributor with this id"}
-					return Response(content, status=status.HTTP_404_NOT_FOUND)
+					content = {"detail":"only admin acount can delete contributors"}
+					return Response(content)
 			else:
-				content = {"detail":"only admin acount can delete contributors"}
-				return Response(content)
+				content = {"detail":"Not found project with this id"}
+				return Response(content, status=status.HTTP_404_NOT_FOUND)
 		except:
 			return Response(status=status.HTTP_404_NOT_FOUND)
 
